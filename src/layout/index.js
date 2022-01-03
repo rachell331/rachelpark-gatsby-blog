c4c8830d64c20967ab8003b92d4e1eaa6e72e237
standup8 <borapark13@gmail.com>
2022-01-03 20:23:49 +0900

    builde error fixed

--- src/layout/index.js
+++ src/layout/index.js
@@ -5,7 +5,7 @@ import PageFooter from '../components/page-footer';
 import ThemeSwitch from '../components/theme-switch';
 import './style.scss';
 
-const Layout = ({ children,location }) => {
+const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -22,11 +22,11 @@ const Layout = ({ children,location }) => {
     }
   `);
   const { title, author } = data.site.siteMetadata;
-
+  
   return (
     <div className="page-wrapper">
       <PageHeader siteTitle={title || `Title`} />
-      <main className={location.pathname === "" || location.pathname === "abouts" ? "page-content" : "post-content"}>{children}</main>
+      <main className="page-content">{children}</main>
       <PageFooter
         author={author.name || `Author`}
         githubUrl={author.social?.github || `https://www.github.com`}
